dab34a0c452f32480e686970700f50cc01a894a9
Wesley Young <I97143@xactware.com>
2016-06-08 09:15:58 -0600

    Added event-creator-controller

---
+++ client/js/controllers/events-controller.js
@@ -0,0 +1,19 @@
+/**
+ * Created by I97143 on 6/8/2016.
+ */
+(function(){
+    'use strict';
+    
+    angular.module('event-controller', [])
+        .controller('event-controller', eventController);
+    
+    eventController.$inject=[];
+    
+    function eventController(){
+        var ec = this;
+        
+        
+    }
+    
+    
+})();
