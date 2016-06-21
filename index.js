5c5d2c2b2b91326d8b068dc804118b01112cef02
WesleyYoung <wesley.young.portfolio@gmail.com>
2016-06-21 12:02:57 -0600

    added more inputs to the form

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 /**
  * Created by I97143 on 6/8/2016.
  */
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(__dirname + '/client'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+var async = require('async');
+var http = require('http');
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
+
+
+var port = 3343;
+server.listen(port, function() {
+    console.log(`App listening on port ${port}...`);
+});
